2dbbae93f595c60700cee8cc1ef46aee32692e29
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-10-19 11:32:08 +0300

    proxy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+let isAdmin = true;
+
+const person = {
+  id: 1,
+  name: "Piyush Garg",
+  age: 22,
+  balance: 100,
+};
+
+const personProxy = new Proxy(person, {
+  set: (obj, prop, value) => {
+    if (prop === "name") {
+      obj[prop] = value;
+    } else {
+      throw new Error("You can only change name property");
+    }
+  },
+  get: (obj, prop) => {
+    if (prop === "balance" && isAdmin) return obj[prop];
+  },
+});
 
 function App() {
+  const [state, setState] = useState(1);
+
   return (
     <div className="App">
       <header className="App-header">
+        <h1>{personProxy.id}</h1>
+        <h1>{personProxy.name}</h1>
+        <h1>{personProxy.balance}</h1>
+        <button
+          onClick={(e) => {
+            personProxy.id = 2;
+            setState((s) => s + 1);
+          }}
+        >
+          Change
+        </button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
